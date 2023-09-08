909e0344bfe9e61820a2e1b62faffb11000d0ef7
Cristian Llanos <cristian.llanos@gmail.com>
2023-09-07 21:16:00 -0300

    service layer

--- src/main/java/cl/coopeuch/tasks/service/ITaskService.java
+++ src/main/java/cl/coopeuch/tasks/service/ITaskService.java
@@ -3,7 +3,13 @@ package cl.coopeuch.tasks.service;
 import cl.coopeuch.tasks.entity.TaskEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface ITaskService {
+    List<TaskEntity> listTasks();
     TaskEntity createTask(TaskEntity task);
+    TaskEntity readTask(Long taskId);
+    TaskEntity updateTask(Long taskId, TaskEntity newTask);
+    void deleteTask(Long taskId);
 }
--- src/main/java/cl/coopeuch/tasks/service/impl/TaskService.java
+++ src/main/java/cl/coopeuch/tasks/service/impl/TaskService.java
@@ -6,7 +6,11 @@ import cl.coopeuch.tasks.service.ITaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
 @Transactional
@@ -19,4 +23,33 @@ public class TaskService implements ITaskService {
     public TaskEntity createTask(TaskEntity task) {
         return taskRepository.save(task);
     }
+
+    @Override
+    public TaskEntity readTask(Long taskId) {
+        return taskRepository.findById(taskId)
+                .orElseThrow(() ->
+                new EntityNotFoundException(String.format("task id %d not found.", taskId)));
+    }
+
+    @Override
+    public List<TaskEntity> listTasks() {
+        return StreamSupport.stream(taskRepository.findAll().spliterator(), false)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public TaskEntity updateTask(Long taskId, TaskEntity newTask) {
+        TaskEntity update = this.readTask(taskId);
+        update.setDescription(newTask.getDescription());
+        update.setCreation(newTask.getCreation());
+        update.setVigency(newTask.getVigency());
+        return taskRepository.save(update);
+    }
+
+
+    @Override
+    public void deleteTask(Long taskId) {
+        taskRepository.deleteById(taskId);
+    }
+
 }
